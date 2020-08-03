c110a6e59bcbc5f22a0962ee648805373e7d7a08
Peter Boomsma <peter.boomsma@wijzijnumbrella.nl>
2020-08-03 22:43:23 +0200

    Movies are placed in multiple ul elements to fit the screen

--- src/components/Dashboard/MovieOverview/MovieOverview.tsx
+++ src/components/Dashboard/MovieOverview/MovieOverview.tsx
@@ -1,8 +1,10 @@
-import React, {useContext} from 'react';
+/* eslint-disable */
+import React, {useContext, useEffect, useState} from 'react';
 import {MovieContext} from '../../../context/MovieContext';
 import MovieOnDashboard from '../MovieOnDashboard/MovieOnDashboard';
 import styled from 'styled-components';
 import {IMovie} from '../../../movieseat';
+import {chunk} from 'lodash';
 
 const MovieList = styled.ul`
   list-style: none;
@@ -11,16 +13,37 @@ const MovieList = styled.ul`
   margin: 0 12px;
 `;
 
+
 const MovieOverview = () => {
+  const [movieRows, setMovieRows] = useState<IMovie[][]>([]);
   const [movies] = useContext(MovieContext);
+  let numberOfMovies: number;
+
+  useEffect(() => {
+    setMoviesInRow()
+    window.addEventListener("resize", () => {
+      setMoviesInRow()
+    });
+  }, []);
+
+  const setMoviesInRow = () => {
+    numberOfMovies = Math.floor((window.innerWidth -24) / 185);
+    setMovieRows(chunk(movies, numberOfMovies));
+  }
 
   return (
-    <MovieList>
-      { movies ? movies.map((movie: IMovie) => (
-        <MovieOnDashboard key={movie.id} movie={movie}/>
-      )) : null}
-    </MovieList>
+    <div>
+      { movieRows ? movieRows.map((movieRow, index) => (
+        <MovieList key={index}>
+          { movieRow.map((movie: IMovie) => (
+            <MovieOnDashboard key={movie.id} movie={movie}/>
+          ))}
+        </MovieList>
+      )): null}
+    </div>
   );
 };
 
 export default MovieOverview;
+
+// 12 x 185 = 2220
