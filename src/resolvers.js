450c6f6841a2b4b8ab8f6a24055a34e98cbd0ca6
Peter Boomsma <peter@cleanpixel.nl>
2021-08-29 21:12:17 +0200

    removed student refs

--- src/resolvers.js
+++ src/resolvers.js
@@ -2,14 +2,6 @@
 
 const {prisma} = require('./database.js');
 
-const Student = {
-  id: (parent) => parent.id,
-  email: (parent) => parent.email,
-  fullName: (parent) => parent.fullName,
-  dept: (parent) => parent.dept,
-  enrolled: (parent) => parent.enrolled,
-};
-
 const Movie = {
   id: (parent) => parent.id,
   original_title: (parent) => parent.original_title,
@@ -18,22 +10,6 @@ const Movie = {
 };
 
 const Query = {
-  numberSix() {
-    return 6;
-  },
-  enrollment: () => {
-    return prisma.student.findMany({
-      where: {enrolled: true},
-    });
-  },
-  students: () => {
-    return prisma.student.findMany({});
-  },
-  student: (parent, args) => {
-    return prisma.student.findFirst({
-      where: {id: Number(args.id)},
-    });
-  },
   movie: () => {
     return prisma.movie;
   },
@@ -43,24 +19,6 @@ const Query = {
 };
 
 const Mutation = {
-  registerStudent: (parent, args) => {
-    return prisma.student.create({
-      data: {
-        email: args.email,
-        fullName: args.fullName,
-        dept: args.dept,
-        enrolled: args.enrolled,
-      },
-    });
-  },
-  enroll: (parent, args) => {
-    return prisma.student.update({
-      where: {id: Number(args.id)},
-      data: {
-        enrolled: true,
-      },
-    });
-  },
   addMovie: (parent, args) => {
     return prisma.movie.create({
       data: {
@@ -80,7 +38,7 @@ const Mutation = {
   },
 };
 
-const resolvers = {Student, Movie, Query, Mutation};
+const resolvers = {Movie, Query, Mutation};
 
 module.exports = {
   resolvers,
--- src/schema.js
+++ src/schema.js
@@ -4,14 +4,6 @@ const {gql} = require('apollo-server');
 
 const typeDefs = gql`
 
-  type Student {
-    id: ID!
-    email: String!
-    fullName: String!
-    dept: String
-    enrolled: Boolean
-  }
-
   type Movie {
     id: ID!
     original_title: String!
@@ -24,19 +16,11 @@ const typeDefs = gql`
   }
 
   type Query {
-    enrollment: [Student!]
-    students: [Student!]!
-    student(id: ID!): Student
     movie(id: ID!): Movie
     movies: [Movie!]!
-    numberSix: Int!
   }
 
   type Mutation {
-    registerStudent(
-      email: String!, fullName: String!, dept: String, enrolled: Boolean
-    ): Student!
-    enroll(id: ID!): Student
     addMovie(
       original_title: String!, 
       tmdb_id: Int!,
