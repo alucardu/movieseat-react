e86fa4b02d8215423308b0231ce50a6cfe69854b
Peter Boomsma <peter@cleanpixel.nl>
2021-10-05 19:03:58 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env) => {
     },
     output: {
       path: __dirname + '/dist',
-      publicPath: env.NODE_ENV === 'development' ? '/' : './',
+      publicPath: env.NODE_ENV === 'development' ? '/' : '/',
       filename: 'bundle.js',
     },
     module: {
