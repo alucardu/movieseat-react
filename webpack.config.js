c7553f62b01affd434c8e9469889c2676575d76a
Peter Boomsma <peter@cleanpixel.nl>
2021-10-05 19:00:18 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,45 @@ const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
-module.exports = {
-  devtool: 'eval-cheap-source-map',
-  entry: './src/index.tsx',
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-  },
-  output: {
-    path: __dirname + '/dist',
-    publicPath: process.env.NODE_ENV === 'development' ? '/' : '',
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
-      },
-    ],
-  },
-  devServer: {
-    historyApiFallback: {
-      index: '/',
+module.exports = (env) => {
+  return {
+    devtool: 'eval-cheap-source-map',
+    entry: './src/index.tsx',
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
     },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new Dotenv(),
-    new ESLintPlugin(),
-  ],
+    output: {
+      path: __dirname + '/dist',
+      publicPath: env.NODE_ENV === 'development' ? '/' : './',
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ],
+        },
+      ],
+    },
+    devServer: {
+      historyApiFallback: {
+        index: '/',
+      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+      new Dotenv(),
+      new ESLintPlugin(),
+    ],
+  };
 };
