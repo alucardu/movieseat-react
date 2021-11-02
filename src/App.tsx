bbe17dc3e84601e2c9bfd687aa19e9afb9c57f38
Peter Boomsma <peter@cleanpixel.nl>
2021-11-03 00:57:42 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -50,15 +50,12 @@ const App = () => {
 
 
   useEffect(() => {
-    window.addEventListener('beforeinstallprompt', (e) => {
-      // Prevent the mini-infobar from appearing on mobile
-      e.preventDefault();
-      // Stash the event so it can be triggered later.
-      // deferredPrompt = e;
-      // // Update UI notify the user they can install the PWA
-      // showInstallPromotion();
-      // Optionally, send analytics event that PWA install promo was shown.
-      console.log(`'beforeinstallprompt' event was fired.`);
+    window.addEventListener('beforeinstallprompt', function(e) {
+      // log the platforms provided as options in an install prompt
+      console.log(e.platforms); // e.g., ["web", "android", "windows"]
+      e.userChoice.then(function(choiceResult) {
+        console.log(choiceResult.outcome); // either "accepted" or "dismissed"
+      });
     });
   }, []);
 
