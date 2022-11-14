5b9a7a68f258a915fc0c76e06d049ad73aed9f15
Peter Boomsma <peter@cleanpixel.nl>
2022-11-14 23:00:48 +0100

    Fixed bug

--- server/updateMovies.mjs
+++ server/updateMovies.mjs
@@ -21,6 +21,7 @@ const config = {
 };
 
 const pool = new pg.Pool(config);
+
 /**
  *
  */
@@ -44,7 +45,6 @@ function updateMovies() {
           json.releases?.countries.forEach((countrie) => {
             if (countrie.iso_3166_1 == 'NL') {
               json.release_date = countrie.release_date;
-              return;
             }
           });
 
@@ -53,7 +53,7 @@ function updateMovies() {
           const changedValues = isValueChanged(movie, json);
 
           if (changedValues.valueChanged) {
-            console.log('changed', json.original_title);
+            console.log('changed', movie.original_title, changedValues.valueChanged);
 
             client.query('UPDATE "Movie" SET original_title = $1::text WHERE tmdb_id = $2::integer', [json.original_title, json.id]);
             client.query('UPDATE "Movie" SET poster_path = $1::text WHERE tmdb_id = $2::integer', [json.poster_path, json.id]);
@@ -74,8 +74,9 @@ function updateMovies() {
           // // console.log('ID: ', json.id, json.videos.results.length);
           json.videos?.results.forEach((movieVideo) => {
             if (movieVideo.type === 'Trailer') {
-              client.query('SELECT * FROM "MovieVideo" WHERE tmdb_id = $1::bigint', [parseInt(movieVideo.id)]).then((result) => {
+              client.query('SELECT * FROM "MovieVideo" WHERE key = $1::text', [movieVideo.key]).then((result) => {
                 if (result.rowCount === 0) {
+                  console.log('added trailer', movie.original_title);
                   client.query('INSERT into "MovieVideo"(iso_639_1, iso_3166_1, name, key, site, size, type, official, published_at, "movieId", tmdb_id)VALUES($1::text, $2::text, $3::text, $4::text, $5::text, $6::integer, $7::text, $8::boolean, $9::text, $10::integer, $11::bigint )',
                       [movieVideo.iso_639_1, movieVideo.iso_3166_1, movieVideo.name, movieVideo.key, movieVideo.site, movieVideo.size, movieVideo.type, movieVideo.official, movieVideo.published_at, movie.id, parseInt(movieVideo.id)], (err, res) => {
                         if (err) throw err;
@@ -116,7 +117,7 @@ function updateMovies() {
     let changedValue = '';
 
     for (const [key, value] of Object.entries(movie)) {
-      if (key !== 'tmdb_id' && key !== 'id' && value !== json[key]) {
+      if (key !== 'tmdb_id' && key !== 'id' && json[key] !== undefined && value !== json[key]) {
         changedValue = key;
         valueChanged = true;
       }
