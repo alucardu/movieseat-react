04b59bc6eb9b03842a79ed01892e3d4e0c91e30d
Peter Boomsma <peter@cleanpixel.nl>
2023-01-18 22:53:02 +0100

    debugger

--- src/components/TrailerSlider/TrailerSlider.tsx
+++ src/components/TrailerSlider/TrailerSlider.tsx
@@ -1,6 +1,5 @@
-import {Button} from '@mui/material';
 import {Box} from '@mui/system';
-import React, {createRef, useEffect, useRef, useState} from 'react';
+import React, {createRef, useEffect, useState} from 'react';
 
 import ReactPlayer from 'react-player';
 import screenfull from 'screenfull';
@@ -37,28 +36,20 @@ export const TrailerSlider = (props: Props) => {
       setIsPlaying(true);
     };
 
-    const handleClick = () => {
-      window.screen.orientation.lock('landscape');
-      // window.screen.orientation.lock('portrait');
-    };
-
     return (
-      <>
-        <Button onClick={handleClick}>CLICK</Button>
-        <Box
-          key={props.video.key}
-          sx={{left: props.index*100 + 'vw'}}>
-          <ReactPlayer
-            ref={element}
-            playing={isPlaying}
-            onPlay = {() => makeFullscreen(element)}
-            onPause = {() => setIsPlaying(false)}
-            controls={true}
-            width="100vw"
-            height="auto"
-            url={`https://www.youtube.com/watch?v=${props.video.key}`} />
-        </Box>
-      </>
+      <Box
+        key={props.video.key}
+        sx={{left: props.index*100 + 'vw'}}>
+        <ReactPlayer
+          ref={element}
+          playing={isPlaying}
+          onPlay = {() => makeFullscreen(element)}
+          onPause = {() => setIsPlaying(false)}
+          controls={true}
+          width="100vw"
+          height="auto"
+          url={`https://www.youtube.com/watch?v=${props.video.key}`} />
+      </Box>
     );
   };
 
